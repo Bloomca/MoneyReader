cdcf3d557b654e698faaec6099ad03523771d2b0
Seva Zaikov <seva.zaikov@gmail.com>
2025-05-03 13:21:12 -0700

    Add ignored prefixes UI

---
+++ MoneyReader/ViewModels/ConfiguratorVM.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoneyReader.Utils;
+
+namespace MoneyReader.ViewModels
+{
+    public class ConfiguratorVM : BaseVM
+    {
+
+        private ObservableCollection<string> _ignoredPrefixes = [];
+        public ObservableCollection<string> IgnoredPrefixes { get =>  _ignoredPrefixes; }
+
+        // TODO: switch to listener on _ignoredPrefixes instead of direct calls
+        public bool HasIgnoredPrefixes {  get => _ignoredPrefixes.Count != 0; }
+
+        public void AddIgnoredPrefix(string prefix)
+        {
+            bool hadNoPrefixes = _ignoredPrefixes.Count == 0;
+            _ignoredPrefixes.Add(prefix);
+
+            if (hadNoPrefixes)
+            {
+                OnPropertyChanged(nameof(HasIgnoredPrefixes));
+            }
+        }
+
+        public void RemoveIgnoredPrefix(string prefix)
+        {
+            _ignoredPrefixes.Remove(prefix);
+
+            bool hasNoPrefixes = _ignoredPrefixes.Count == 0;
+            if (hasNoPrefixes)
+            {
+                OnPropertyChanged(nameof(HasIgnoredPrefixes));
+            }
+        }
+    }
+}
---
+++ MoneyReader/Views/Configurator.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MoneyReader.ViewModels;
+
+namespace MoneyReader.Views
+{
+    /// <summary>
+    /// Interaction logic for Configurator.xaml
+    /// </summary>
+    public partial class Configurator : UserControl
+    {
+        public ConfiguratorVM VM { get; } = new ConfiguratorVM();
+        public Configurator()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void OnAddPrefixClick(object sender, RoutedEventArgs e)
+        {
+            string prefix = IgnoreTextBox.Text;
+
+            if (String.IsNullOrEmpty(prefix))
+            {
+                // Simply ignore the button click for now
+                return;
+            }
+
+            VM.AddIgnoredPrefix(prefix);
+            IgnoreTextBox.Clear();
+        }
+
+        private void OnRemovePrefixClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button { Tag: string prefixToRemove })
+            {
+                VM.RemoveIgnoredPrefix(prefixToRemove);
+            }
+        }
+    }
+}
